de3f809cd79e6858c05a3c2d590ec71e7049ebde
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2026-03-15 15:24:10 +0000

    add button sounds

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,19 @@
 import "./Button.css";
 
+import clickSfx from "../assets/sfx/minecraft_click.mp3";
+
+import { useSettings } from "../utils/SettingsStore.jsx";
+
 export default function Button({ id = "", onclick = () => {}, children }) {
+    const { settings } = useSettings();
+
     return (
-        <div id={id} class="mc-button" onclick={onclick}>
+        <div id={id} class="mc-button" onclick={(e) => {
+            const sound = new Audio(clickSfx);
+            sound.volume = settings.volume / 100;
+            sound.play();
+            onclick(e);
+        }}>
             <div class="title">
                 {children}
             </div>
