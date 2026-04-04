cff7819e0c6a72b5597fbded9841043972ec44a4
TheHuckle <crazyh.dev@gmail.com>
2026-04-04 22:49:41 +0100

    make building easier

--- build-scripts/build-all.sh
+++ build-scripts/build-all.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 echo "Building for all platforms..."
 
+CONF=./neutralino.config.json
+
+if [ ! -e "./${CONF}" ]; then
+    echo
+    echo -e "\033[31m\033[1mERROR: ${CONF} not found.\033[0m"
+    exit 1
+fi
+
+if ! jq -e '.buildScript | has("linux")' "${CONF}" > /dev/null; then
+    echo
+    echo -e "\033[31m\033[1mERROR: Missing buildScript JSON structure in ${CONF}\033[0m"
+    exit 1
+fi
+
+sed -i '' 's/"enableInspector": *true/"enableInspector": false/' "$CONF"
+sed -i '' 's/"tokenSecurity": *"none"/"tokenSecurity": "one-time"/' "$CONF"
+
 chmod +x ./build-scripts/build-linux.sh ./build-scripts/build-mac.sh ./build-scripts/build-win.sh
 
 ./build-scripts/build-linux.sh
 ./build-scripts/build-mac.sh
 ./build-scripts/build-win.sh
 
+sed -i '' 's/"enableInspector": *false/"enableInspector": true/' "$CONF"
+sed -i '' 's/"tokenSecurity": *"one-time"/"tokenSecurity": "none"/' "$CONF"
+
 echo 
 echo -e "\033[1mAll platforms built.\033[0m"
-
-echo -e "\033[1mMAKE SURE THAT tokenSecurity was set to one-time in neutralino CONFIG!\033[0m"
-echo -e "\033[1mAlso disable webInspector prop in neutralino config!\033[0m"
