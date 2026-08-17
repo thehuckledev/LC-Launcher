86ad728bca17ff4d6a5592d18c57b3200379271a
TheHuckle <crazyh.dev@gmail.com>
2026-08-17 23:54:02 +0100

    fix arch naming

--- build-scripts/sharun/make-appimage.sh
+++ build-scripts/sharun/make-appimage.sh
@@ -3,6 +3,14 @@
 set -eu
 
 ARCH=$(uname -m)
+case "$ARCH" in
+    aarch64|arm64)
+        TARGET="arm64"
+        ;;
+    *)
+        TARGET="x64"
+        ;;
+esac
 VERSION="$(tr -d '\r\n' < BUMP)"
 export ARCH VERSION
 export ADD_HOOKS="self-updater.hook"
@@ -12,10 +20,10 @@ build_appimage() {
     VARIANT_SUFFIX="$2"
 
     export OUTPATH="./dist"
-    export OUTNAME="LC-Launcher${VARIANT_SUFFIX}-${ARCH}.AppImage"
+    export OUTNAME="LC-Launcher${VARIANT_SUFFIX}-${TARGET}.AppImage"
     export ICON="./build-scripts/sharun/template/LC-Launcher.png"
     export DESKTOP="./build-scripts/sharun/template/LC-Launcher.desktop"
-    export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*LC-Launcher${VARIANT_SUFFIX}*${ARCH}.AppImage.zsync"
+    export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*LC-Launcher${VARIANT_SUFFIX}*${TARGET}.AppImage.zsync"
 
     quick-sharun "$BINARY_PATH"
     quick-sharun --make-appimage
@@ -25,5 +33,5 @@ build_appimage() {
     rm -rf ./*.AppDir
 }
 
-build_appimage "./dist/linux_${ARCH}/LC-Launcher/LC_Launcher-linux_${ARCH}" ""
-build_appimage "./dist/linux_${ARCH}_portable/LC-Launcher/LC_Launcher-linux_${ARCH}" "-Portable"
+build_appimage "./dist/linux_${TARGET}/LC-Launcher/LC_Launcher-linux_${TARGET}" ""
+build_appimage "./dist/linux_${TARGET}_portable/LC-Launcher/LC_Launcher-linux_${TARGET}" "-Portable"
